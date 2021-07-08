ff478c48e022ae845136c117fdcbb6027ef665f7
Vladislav <vsakayafignavlad@gmail.com>
2021-07-08 16:42:50 +0300

    server first steps'

---
+++ config.py
@@ -0,0 +1,8 @@
+"""
+This is a config file where you can set preferences
+to a server work, such as time of generating new keys
+or the maximum length of query
+"""
+
+time_new_keys = 1  # write in hours
+keys_length = 512
