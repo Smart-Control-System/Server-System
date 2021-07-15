7aae9297a3c9c5a39284011fea9d956bd172511a
Vladislav <vsakayafignavlad@gmail.com>
2021-07-15 21:47:06 +0300

    lifetime test done:

--- query_handler.py
+++ query_handler.py
@@ -11,47 +11,59 @@ class Server:
     def __init__(self, server_ip, port):
 
         self.socket = socket.socket()
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.socket.bind((str(server_ip), int(port)))
         self.socket.listen(1)
         self.connection = None
         self.address = None
         self.buffer_size = 0
         self.json_data = 0
+        # unsafe but for now ok
+        self.data_receive = "bdalbdald"
+
+
 
     def main(self):
+        while 1:
+            self.connection, self.address = self.socket.accept()
+
+            # always setting up buffer size before a message
+            self.buffer_size = self.connection.recv(8).decode('utf-8')
+
+            if self.buffer_size.isdigit():
+                print('setting buffer size to', self.buffer_size)
+            else:
+                self.connection.close()
+                continue
+
+            self.data_receive = self.connection.recv(int(self.buffer_size)).decode('utf-8')
+
+            self.data_receive = json.loads(self.data_receive)
+
+
+            print(f'Message from {str(self.address)}\n'
+                  f'"{self.data_receive}"')
+
+            return float(self.data_receive["data"]["sensor_values"])
+
+            # with open('all_queries.json', 'r') as file:
+            #     string_to_write = json.dumps(self.data_receive) + '\n'
+            #     file.write(str(string_to_write))
+
+
+
+    def return_last_amount_light(self):
+        print(self.data_receive)
         try:
-            while 1:
-                self.connection, self.address = self.socket.accept()
-                print(self.connection)
+            return self.data_receive["data"]
+        except:
+            print('error')
 
-                # always setting up buffer size before a message
-                self.buffer_size = self.connection.recv(8).decode('utf-8')
 
-                if self.buffer_size.isdigit():
-                    print('setting buffer size to', self.buffer_size)
-                else:
-                    self.connection.close()
-                    continue
-
-                data_receive = self.connection.recv(int(self.buffer_size)).decode('utf-8')
-
-                data_receive = json.loads()
-
-                with open('all_queries.json', 'r') as file:
-                    string_to_write = data_receive + '\n'
-                    file.write(string_to_write)
-
-                print(f'Message from {str(self.address)}\n'
-                      f'"{data_receive}"')
-
-        except Exception as ex:
-            print(ex)
-        finally:
-            self.socket.close()
 
 
 
 
 if __name__ == '__main__':
-    a = Server('192.168.1.86', 7777)
+    a = Server('192.168.1.99', 7777)
     a.main()
--- test_lifeplots.py
+++ test_lifeplots.py
@@ -10,45 +10,50 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import random
 
-url = "https://qrng.anu.edu.au/API/jsonI.php?length=1&type=uint8"
+import multiprocessing
 
-# function to pull out a float from the requests response object
-def pull_float(response):
-    jsonr = response.json()
-    strr = jsonr["data"][0]
-    if strr:
-        fltr = round(float(strr), 2)
-        return fltr
-    else:
-        return None
+from query_handler import Server
 
 
-# Create figure for plotting
-fig, ax = plt.subplots()
-xs = []
-ys = []
+def plott():
+    server = Server('192.168.1.100', 6767)
+    # Create figure for plotting
+    fig, ax = plt.subplots()
+    xs = []
+    ys = []
 
-def animate(i, xs:list, ys:list):
-    # grab the data from thingspeak.com
-    flt = random.randint(1,200)
-    # Add x and y to lists
-    xs.append(dt.datetime.now().strftime('%H:%M:%S'))
-    ys.append(flt)
-    # Limit x and y lists to 10 items
-    xs = xs[-100:]
-    ys = ys[-100:]
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-    # Format plot
-    ax.set_ylim([0,255])
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.20)
-    ax.set_title('Plot of random numbers from https://qrng.anu.edu.au')
-    ax.set_xlabel('Date Time (hour:minute:second)')
-    ax.set_ylabel('Random Number')
+    def animate(i, xs: list, ys: list):
+        # grab the data from thingspeak.com
+        flt = server.main()
+        # Add x and y to lists
+        xs.append(dt.datetime.now().strftime('%H:%M:%f'))
+        ys.append(flt)
+        # Limit x and y lists to 10 items
+        xs = xs[-20:]
+        ys = ys[-20:]
+        # Draw x and y lists
+        ax.clear()
+        ax.plot(xs, ys)
+        # Format plot
+        ax.set_ylim([0,100])
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.20)
+        ax.set_title('Plot of random numbers from https://qrng.anu.edu.au')
+        ax.set_xlabel('Date Time (hour:minute:second)')
+        ax.set_ylabel('Random Number')
 
-# Set up plot to call animate() function every 1000 milliseconds
-ani = animation.FuncAnimation(fig, animate, fargs=(xs,ys), interval=200)
+    # Set up plot to call animate() function every 1000 milliseconds
+    ani = animation.FuncAnimation(fig, animate, fargs=(xs, ys), interval=100)
 
-plt.show()
+    plt.show()
+
+plott()
+# if __name__ == "__main__":
+#     plot_thread = multiprocessing.Process(target=plott)
+#     plot_thread.start()
+#     print('plots started')
+#
+#
+#     server_thread = multiprocessing.Process(target=server.main)
+#     server_thread.start()
+#     print('server started')
