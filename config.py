c0001055d01ef753a60e8fdf1c29980ffe8075f4
Vladislav <vsakayafignavlad@gmail.com>
2021-08-04 20:04:01 +0300

    We need to reboot a bit

--- config.py
+++
@@ -1,8 +0,0 @@
-"""
-This is a config file where you can set preferences
-to a server work, such as time of generating new keys
-or the maximum length of query
-"""
-
-time_new_keys = 1  # write in hours
-keys_length = 512
