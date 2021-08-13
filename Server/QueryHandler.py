6ebb4eb6bcf1713a5237d9792c40a6f030c744b5
Vladislav <vsakayafignavlad@gmail.com>
2021-08-13 16:40:12 +0300

    check if client wants to dissconnect

--- Server/QueryHandler.py
+++ Server/QueryHandler.py
@@ -43,9 +43,10 @@ class QueryHandler:
                 self.db.connect('allq')
                 self.db.write_query(self.data_receive)
                 data = self.data_receive.split('_')
+
                 if data[0] == '0':
                     try:
-                        self.connection.send(self.commands[data[1]].encode('utf-8'))
+                        self.connection.send(self.commands[data[1]].pop().encode('utf-8'))
                     except IndexError:
                         pass  # if there is no commands for object just do nothing
                     if data[1] in self.clients_requests.keys():
@@ -53,11 +54,22 @@ class QueryHandler:
                             sock_for_client = socket.socket()
                             sock_for_client.connect((address, 6767))
                             sock_for_client.send(self.data_receive.encode('utf-8'))
+
+                # if client need to get data from obj
                 elif data[0] == '1':
-                    try:
-                        self.clients_requests[data[1]] += self.address
-                    except KeyError:
-                        self.clients_requests[data[1]] = self.address
+                    # adding or deleting client
+                    if data[2] == '1':
+                        try:
+                            self.clients_requests[data[1]] += self.address
+                        except KeyError:
+                            self.clients_requests[data[1]] = self.address
+                    elif data[2] == '0':
+                        try:
+                            self.clients_requests[data[1]].remove(self.address)
+                        except ValueError:
+                            pass
+
+                # if client need to send come setting command
                 elif data[0] == '2':
                     try:
                         self.commands[data[1]] += data[3]
